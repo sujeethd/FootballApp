981fa668fa5e3c067433ff56603caf58232fba7c
Sujeeth Dharanikota <sujeethdh@gmail.com>
2026-01-31 20:49:34 -0500

    Fix config for VsCode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
+  
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
@@ -16,7 +15,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 
